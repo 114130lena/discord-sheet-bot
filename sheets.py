310c170949b52fd4678f9114756d0a1c88053245
114130lena <xogusw177@gmail.com>
2026-08-31 06:01:51 +0900

    sheets.py

--- sheets.py
+++ sheets.py
@@ -2,14 +2,12 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 
-# Google Sheets / Drive 권한
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
 ]
 
 
-# 서비스 계정 인증
 credentials = Credentials.from_service_account_file(
     "google-service-account.json",
     scopes=SCOPES
@@ -19,17 +17,15 @@ gc = gspread.authorize(credentials)
 
 
 def create_spreadsheet(project):
-    """
-    프로젝트 데이터를 새로운 Google 스프레드시트로 만든다.
-    """
 
     spreadsheet_name = "전력분석"
 
-    spreadsheet = gc.create(spreadsheet_name)
+    spreadsheet = gc.create(
+        spreadsheet_name
+    )
 
     worksheet = spreadsheet.sheet1
 
-    # 헤더
     headers = [
         "팀명",
         "로스터",
@@ -42,7 +38,6 @@ def create_spreadsheet(project):
 
     worksheet.append_row(headers)
 
-    # 팀 데이터
     for team in project.get("teams", []):
 
         roster_size = team.get(
@@ -55,7 +50,6 @@ def create_spreadsheet(project):
             ""
         )
 
-        # 3인 로스터면 선수4 비우기
         if roster_size == 3:
             player4 = ""
 
@@ -71,13 +65,10 @@ def create_spreadsheet(project):
 
         worksheet.append_row(row)
 
-    # 헤더 고정
     worksheet.freeze(rows=1)
 
-    # 필터
     worksheet.set_basic_filter()
 
-    # 열 너비
     worksheet.format(
         "A:G",
         {
@@ -86,7 +77,6 @@ def create_spreadsheet(project):
         }
     )
 
-    # 헤더 굵게
     worksheet.format(
         "A1:G1",
         {
